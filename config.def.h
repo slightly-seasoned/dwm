a9a7da354b6e26e3ce81c6f054e3b864bf749a2a
slightly-seasoned <brandondp723@gmail.com>
2022-06-25 20:17:08 -0230

    Applied underlinetags patch.

--- config.def.h
+++ config.def.h
@@ -40,6 +40,11 @@ static const int  tagschemes[] = { SchemeTag1, SchemeTag2, SchemeTag3,
                                    SchemeTag4, SchemeTag5, SchemeTag6,
                                    SchemeTag7, SchemeTag8, SchemeTag9 };
 
+static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
